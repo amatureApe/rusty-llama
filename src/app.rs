a73ac6f0b488de6f449dcdd6b67a97fbc56d4116
amatureApe <amatureApe@gmail.com>
2024-07-18 22:33:48 +0800

    cleanup

--- src/app.rs
+++ src/app.rs
@@ -21,12 +21,33 @@ pub fn App() -> impl IntoView {
                 user: true,
             };
             set_conversation.update(move |c| {
-                c.message.push(user_message);
+                c.messages.push(user_message);
             });
 
             converse(conversation.get())
         }
-    })
+    });
+
+    create_effect(move |_| {
+        if let Some(_) = send.input().get() {
+            let model_message = Message {
+                text: String::from("..."),
+                user: false,
+            };
+
+            set_conversation.update(move |c| {
+                c.messages.push(model_message);
+            });
+        }
+    });
+
+    create_effect(move |_| {
+        if let Some(Ok(response)) = send.value().get() {
+            set_conversation.update(move |c| {
+                c.messages.last_mut().unwrap().text = response;
+            });
+        }
+    });
 
     view! {
         // injects a stylesheet into the document <head>
